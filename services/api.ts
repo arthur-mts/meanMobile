fcc1997d69537cdc3c2c7ecd13d310fe13a9ba5e
Arthur Mauricio <arthur.mauricio@academico.ifpb.edu.br>
2021-01-29 22:17:44 -0300

    fix api endpoint

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // import {API_URL} from '@env';
 
-const API_URL = 'https://golaroo.com/api';
+const API_URL = 'https://golaroo.com/api/accounts';
 
 export async function postNotification(userName: string) {
   console.log(`${API_URL}/${userName}/notification`);
